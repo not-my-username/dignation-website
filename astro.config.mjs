0aa2108304bfdc1d029c6b83f92fbea23d7bf8c0
Liam Sherwin <something.random.i.made.at.3am@gmail.com>
2024-09-01 22:59:02 +1200

    Revert "Updated Members Grid"
    This reverts commit 2d832faa78f6028b732754882739b99f5e4fdba8.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,30 +3,30 @@ import starlight from '@astrojs/starlight';
 
 import netlify from '@astrojs/netlify';
 
-import tailwind from '@astrojs/tailwind';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.dignation.nz',
 
-  integrations: [starlight({
-      title: 'DigNation',
-      social: {
-          github: 'https://github.com/dignation/website',
-      },
-      sidebar: [
-          {
-              label: 'Getting started',
-              items: [
-                  // Each item here is one entry in the navigation menu.
-                  { label: 'How to join', slug: 'getting-started/how-to-join' },
-              ],
+  integrations: [
+      starlight({
+          title: 'DigNation',
+          social: {
+              github: 'https://github.com/dignation/website',
           },
-      ],
-      logo: {
-          src: './src/assets/logo.png',
-        },
-  }), tailwind()],
+          sidebar: [
+              {
+                  label: 'Getting started',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'How to join', slug: 'getting-started/how-to-join' },
+                  ],
+              },
+          ],
+          logo: {
+              src: './src/assets/logo.png',
+            },
+      }),
+	],
 
   output: 'server',
   adapter: netlify(),
